4908f6441a11bad4c791bd660e4aeffbb1084dc8
lgsosa <lg.sosa@alumno.um.edu.ar>
2024-08-17 22:04:37 -0300

    tests corregidos

--- ajedrez/pieces.py
+++ ajedrez/pieces.py
@@ -1,29 +1,25 @@
-class Piece:    #Clase Padre
+class Piece:  # Clase Padre
     def __init__(self, color):
         self.__color__ = color
 
-    @property             #Me toma como objeto privado el color entonces vi que si hago @property lo solucionaba
-    
+    @property
     def color(self):
         return self.__color__
 
-###Tipo de Piezas###
-
-###TORRES###
-class Rook(Piece):     
+### TORRES ###
+class Rook(Piece):
     def __init__(self, color):
-        super().__init__(color) 
+        super().__init__(color)
 
     def movimientos_basicos_de_torres(self, row, col):
-
         moves = []
 
-        # Muestra los Movimientos verticales (arriba y abajo) a los que poder mover tu torre
+        # Movimientos verticales (arriba y abajo)
         for r in range(8):
             if r != row:
                 moves.append((r, col))
 
-        # Movimientos los horizontales (izquierda y derecha) a los que puede mover tu torre
+        # Movimientos horizontales (izquierda y derecha)
         for c in range(8):
             if c != col:
                 moves.append((row, c))
@@ -31,67 +27,67 @@ class Rook(Piece):
         return moves
 #############
 
-###ALFILS###
+### ALFILS ###
 class Alfils(Piece):
-    
     def movimientos_basicos_de_alfiles(self, row, col):
-        
         moves = []
 
-        #Voy a marcar las "row" como r para mayor comodidad y "col" como c
-
-        ###ARRIBA IZQUIERDA###
-
-        r, c = row -1, col -1 #Les resto 1 para hacer los movimentos arriba izquierda
-        while r >=0 and c >=0:#Me permite hacer el movimiento las veces que quiera hasta que me salga del tablero en ese caso se corta el bucle
-            moves.append((r, c)) #Los agrego
+        ### ARRIBA IZQUIERDA ###
+        r, c = row - 1, col - 1
+        while r >= 0 and c >= 0:
+            moves.append((r, c))
             r -= 1
             c -= 1
 
-        ###ARRIBA DERECHA###
-
-        r, c = row -1, col +1
-        while r >=0 and c <=7:
-            moves.append((r, c)) 
+        ### ARRIBA DERECHA ###
+        r, c = row - 1, col + 1
+        while r >= 0 and c <= 7:
+            moves.append((r, c))
             r -= 1
             c += 1
 
-        ###ABAJO IZQUIERDA###
-
-        r, c = row +1, col -1
-        while r <=7 and c >=0:
-            moves.append((r, c)) 
+        ### ABAJO IZQUIERDA ###
+        r, c = row + 1, col - 1
+        while r <= 7 and c >= 0:
+            moves.append((r, c))
             r += 1
             c -= 1
 
-        ###ABAJO DERECHA###
-
-        r, c = row +1, col +1
-        while r <=7 and c <=7:
-            moves.append((r, c)) 
+        ### ABAJO DERECHA ###
+        r, c = row + 1, col + 1
+        while r <= 7 and c <= 7:
+            moves.append((r, c))
             r += 1
             c += 1
 
         return moves
-    
 #############
 
-###REYES###
+### REYES ###
 class Knight(Piece):
     pass
 #############
 
-###REINAS###
+### REINAS ###
 class Queen(Piece):
     pass
 #############
 
-###CABALLOS###
+### CABALLOS ###
 class Horse(Piece):
     pass
 #############
 
-###PEONES###
-class Pawn(Piece):     
+### PEONES ###
+class Pawn(Piece):
     pass
 #############
+
+if __name__ == '__main__':
+    rook = Rook('white')
+    print("Movimientos básicos de la torre desde la posición (0, 0):")
+    print(rook.movimientos_basicos_de_torres(0, 0))
+
+    alfils = Alfils('black')
+    print("\nMovimientos básicos de los alfiles desde la posición (3, 3):")
+    print(alfils.movimientos_basicos_de_alfiles(3, 3))
--- test/test_pieces.py
+++ test/test_pieces.py
@@ -1,70 +1,21 @@
-from ajedrez.pieces import Rook,Alfils, Piece
+import unittest
+from ajedrez.pieces import Rook, Alfils
 
+class TestRook(unittest.TestCase):
+    def test_movimientos_basicos_de_torres(self):
+        rook = Rook("blanco")
+        self.assertEqual(rook.color, "blanco")
+        expected_moves = [(i, 0) for i in range(8) if i != 0] + [(0, i) for i in range(8) if i != 0]
+        self.assertEqual(rook.movimientos_basicos_de_torres(0, 0), expected_moves)
 
-from typing import List, Tuple
-
-class Test_Piece:
-    def __init__(self, color):
-        self.__color__ = color
-
-    @property
-    def color(self):
-        return self.__color__
-
-class Test_Rook(Piece):
-    def __init__(self, color):
-        super().__init__(color)
-
-    def movimientos_basicos_de_torres(self, row: int, col: int) -> List[Tuple[int, int]]:
-        moves = []
-        for r in range(8):
-            if r != row:
-                moves.append((r, col))
-        for c in range(8):
-            if c != col:
-                moves.append((row, c))
-        return moves
-
-class Test_Alfils(Piece):
-    def movimientos_basicos_de_alfiles(self, row: int, col: int) -> List[Tuple[int, int]]:
-        moves = []
-        r, c = row -1, col -1
-        while r >=0 and c >=0:
-            moves.append((r, c))
-            r -= 1
-            c -= 1
-        r, c = row -1, col +1
-        while r >=0 and c <=7:
-            moves.append((r, c))
-            r -= 1
-            c += 1
-        r, c = row +1, col -1
-        while r <=7 and c >=0:
-            moves.append((r, c))
-            r += 1
-            c -= 1
-        r, c = row +1, col +1
-        while r <=7 and c <=7:
-            moves.append((r, c))
-            r += 1
-            c += 1
-        return moves
-
-def test_rook():
-    r = Rook("blanco")
-    assert r.color == "blanco", "Test Case Rook 1 Failed"
-    expected_moves = [(i, 0) for i in range(8) if i != 0] + [(0, i) for i in range(8) if i != 0]
-    assert r.movimientos_basicos_de_torres(0, 0) == expected_moves, "Test Case Rook 2 Failed"
-
-def test_alfils():
-    a = Alfils("negro")
-    assert a.color == "negro", "Test Case Alfils 1 Failed"
-    expected_moves = [
-        (i, j) for i in range(8) for j in range(8) if abs(i - 3) == abs(j - 3) and (i, j) != (3, 3)
-    ]
-    assert set(a.movimientos_basicos_de_alfiles(3, 3)) == set(expected_moves), "Test Case Alfils 2 Failed"
+class TestAlfils(unittest.TestCase):
+    def test_movimientos_basicos_de_alfiles(self):
+        alfils = Alfils("negro")
+        self.assertEqual(alfils.color, "negro")
+        expected_moves = [
+            (i, j) for i in range(8) for j in range(8) if abs(i - 3) == abs(j - 3) and (i, j) != (3, 3)
+        ]
+        self.assertEqual(set(alfils.movimientos_basicos_de_alfiles(3, 3)), set(expected_moves))
 
 if __name__ == '__main__':
-    test_rook()
-    test_alfils()
-    print("Todos los test pasaron")
+    unittest.main()
