a204c459d0809139c06e312dca420d4dbcb01bae
lgsosa <lg.sosa@alumno.um.edu.ar>
2024-10-04 11:33:51 -0300

    changes

--- ajedrez/all_pieces/knight.py
+++ ajedrez/all_pieces/knight.py
@@ -1,5 +1,4 @@
 from ajedrez.pieces import Piece
-from directions import get_directions
 
 class Knight(Piece):
     def __init__(self, color, row=None, col=None):
@@ -7,16 +6,18 @@ class Knight(Piece):
 
     def basic_knight_moves(self, row, col):
         moves = []
-        directions = get_directions("knight")
-        return self.generate_moves(row, col, directions)
+        possible_moves = [
+        (-2, -1), (-1, -2), (1, -2), (2, -1),
+        (2, 1), (1, 2), (-1, 2), (-2, 1)
+    ]
     
-    def generate_moves(self, row, col, directions):
-        moves = []
-        for direction_r, direction_c in directions:
-            r, c = row + direction_r, col + direction_c
-            if 0 <= r < 8 and 0 <= c < 8:
-                moves.append((r, c))
+        for direction_r , direction_c in possible_moves:
+            r , c = row + direction_r, col + direction_c
+            if 0<= r < 8 and 0 <= c <=8: #in the board
+                moves.append((r,c))
+            
         return moves
     
     def get_possible_moves(self, board, row, col):
         return self.basic_knight_moves(row, col)
+#############
