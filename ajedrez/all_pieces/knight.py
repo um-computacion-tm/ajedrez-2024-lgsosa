364d1514cfddd3f6c5f0d3835683ed01593220c6
lgsosa <lg.sosa@alumno.um.edu.ar>
2024-10-18 11:57:35 -0300

    cambie la logica de knight para que no tenga issues y king y queen, ademas de agregar move_helper para eliminar los issues(probando)

--- ajedrez/all_pieces/knight.py
+++ ajedrez/all_pieces/knight.py
@@ -4,33 +4,9 @@ class Knight(Piece):
     def __init__(self, color, row=None, col=None):
         super().__init__(color, "♘", "♞", row, col)
 
-    def basic_knight_moves(self):
-        moves = []
-        current_position = (self.row, self.col)
-
-        # Lista de movimientos posibles del caballero
-        possible_moves = [
-            (current_position[0] - 2, current_position[1] - 1),
-            (current_position[0] - 1, current_position[1] - 2),
-            (current_position[0] + 1, current_position[1] - 2),
-            (current_position[0] + 2, current_position[1] - 1),
-            (current_position[0] + 2, current_position[1] + 1),
-            (current_position[0] + 1, current_position[1] + 2),
-            (current_position[0] - 1, current_position[1] + 2),
-            (current_position[0] - 2, current_position[1] + 1),
-        ]
-
-        # Filtrar movimientos válidos dentro del tablero
-        for move in possible_moves:
-            if 0 <= move[0] < 8 and 0 <= move[1] < 8:
-                moves.append(move)
-
-        return moves
-
     def get_possible_moves(self, board, row, col):
-        # Actualizar la posición de la pieza antes de calcular los movimientos
-        self.row = row
-        self.col = col
-        
-        # Obtener movimientos básicos sin restricción de otras piezas
-        return self.basic_knight_moves()
+        moves = [
+            (row - 2, col - 1), (row - 1, col - 2), (row + 1, col - 2), (row + 2, col - 1),
+            (row + 2, col + 1), (row + 1, col + 2), (row - 1, col + 2), (row - 2, col + 1)
+        ]
+        return [(r, c) for r, c in moves if 0 <= r < 8 and 0 <= c < 8]
--- ajedrez/all_pieces/queen.py
+++ ajedrez/all_pieces/queen.py
@@ -1,31 +1,13 @@
 from ajedrez.pieces import Piece
+from ajedrez.move_helper import MoveHelper
 
 class Queen(Piece):
     def __init__(self, color, row=None, col=None):
         super().__init__(color, "♕", "♛", row, col)
 
-
-    def basic_queen_moves(self, row, col):
-        moves = []
-        
-        # Definir las direcciones en las que se puede mover la reina
+    def get_possible_moves(self, board, row, col):
         directions = [
             (-1, -1), (-1, 1), (1, -1), (1, 1),  # Diagonales
             (-1, 0), (1, 0), (0, -1), (0, 1)    # Verticales y horizontales
         ]
-        
-        for direction_r, direction_c in directions:
-            r, c = row + direction_r, col + direction_c
-            
-            # Continuar en esa dirección mientras estemos dentro del tablero
-            while 0 <= r < 8 and 0 <= c < 8:
-                moves.append((r, c))
-                
-                # Moverse más en la misma dirección (como la reina puede avanzar varias casillas)
-                r += direction_r
-                c += direction_c
-
-        return moves
-    
-    def get_possible_moves(self, board, row, col):
-        return self.basic_queen_moves(row, col)
+        return MoveHelper.generate_moves(directions, row, col)
