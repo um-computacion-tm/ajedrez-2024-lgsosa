f35bce8918026c5dc23fe9753342f2d5ceb8f8b7
lgsosa <lg.sosa@alumno.um.edu.ar>
2024-10-18 08:40:55 -0300

    Changes again in King, Queen, and Knight, and Pieces; added a class for the directions of these.

--- ajedrez/all_pieces/king.py
+++ ajedrez/all_pieces/king.py
@@ -1,24 +1,9 @@
-from ajedrez.pieces import Piece
+from ajedrez.pieces import Piece, PieceMover
 
 class King(Piece):
     def __init__(self, color, row=None, col=None):
         super().__init__(color, "♔", "♚", row, col)
 
-    def basic_king_moves(self, row, col):
-        moves = []
-        directions = [
-            (-1, -1), (-1, 0), (-1, 1),
-            ( 0, -1),         ( 0, 1),
-            ( 1, -1), ( 1, 0), ( 1, 1)
-        ]
-
-        for direction_row, direction_col in directions:
-            r, c = row + direction_row, col + direction_col
-
-            if self.is_within_bounds(r, c):
-                moves.append((r, c))
-                
-        return moves
-    
     def get_possible_moves(self, board, row, col):
-        return self.basic_king_moves(row, col)
+        directions = PieceMover.get_directions('all_directions')
+        return PieceMover.generate_moves(row, col, directions, board, max_steps=1)  # Solo puede moverse 1 casilla
--- ajedrez/all_pieces/knight.py
+++ ajedrez/all_pieces/knight.py
@@ -1,23 +1,9 @@
-from ajedrez.pieces import Piece
+from ajedrez.pieces import Piece, PieceMover
 
 class Knight(Piece):
     def __init__(self, color, row=None, col=None):
         super().__init__(color, "♘", "♞", row, col)
 
-    def basic_knight_moves(self, row, col):
-        moves = []
-        possible_moves = [
-        (-2, -1), (-1, -2), (1, -2), (2, -1),
-        (2, 1), (1, 2), (-1, 2), (-2, 1)
-    ]
-    
-        for direction_r , direction_c in possible_moves:
-            r , c = row + direction_r, col + direction_c
-            if 0<= r < 8 and 0 <= c <=8: #in the board
-                moves.append((r,c))
-            
-        return moves
-    
     def get_possible_moves(self, board, row, col):
-        return self.basic_knight_moves(row, col)
-#############
+        directions = PieceMover.get_directions('knight_moves')
+        return PieceMover.generate_moves(row, col, directions, board, max_steps=1)  # Caballo solo se mueve 1 paso
